bf13d46f2f2c11b401c460ca06002fba4830c93f
Morgan Bailey <morgan.colbert@myemail.indwes.edu>
2015-10-16 09:10:23 -0400

    Create morgan.bailey.RPMA.asm

---
+++ morgan.bailey.RPMA.asm
@@ -0,0 +1,38 @@
+; Russian Peasent Multiplication   (morgan.bailey.RPMA.asm)
+; Start with 3 memory locations. Call them A, B and Result. 
+; Here is the Algorithm. 
+; For any given A and B: 
+; Start with result = 0. 
+;	While B > 0:
+;		if B is ODD:
+;			Result = Result + A
+;		A = A * 2
+;		B = B // 2 (Integer divide)
+; At the end, Result should equal A * B
+;-------------------------------------------------------------
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+A DWORD 57			; declare variable A
+B DWORD 86			; declare variable B
+result DWORD 0			; declare result
+
+.code
+main PROC
+	mov eax,A			; set A to EAX
+	mov ebx,B			; set B to EBX
+	mov edx,result		; set result EDX
+	.WHILE B > 0		; while B is less than 0
+		shl eax,2			; double A
+		shr ebx,2			; half B
+		.IF ; if B is odd	; test parity flag
+			add edx,eax		; set result to result + A
+		.ENDIF
+	.ENDW
+	invoke ExitProcess,0
+main endp
+end main
